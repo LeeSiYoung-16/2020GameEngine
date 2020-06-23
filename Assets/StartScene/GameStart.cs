5104da8c65bc9f61a3995764fe55536c7c1a0482
LeeSiYoung-16 <tldud8296@gmail.com>
2020-06-24 01:22:55 +0900

    로고씬에서스페이스바누르면게임시작

---
+++ Assets/StartScene/GameStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameStart : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            ChangeScene();
+    }
+}
