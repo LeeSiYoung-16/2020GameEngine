4ca407a00f6145178845bfcc122412052dc2b295
LeeSiYoung-16 <tldud8296@gmail.com>
2020-06-28 20:53:05 +0900

    불꽃Sprite&Animation추가

--- Assets/Stage1/Recipe.cs
+++ Assets/Stage1/Recipe.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class Recipe : MonoBehaviour
 {
+    private float fAddTime = 0f;
+    
+    // 처음에 2개 10초에 하나씩
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,17 @@ public class Recipe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        fAddTime += Time.deltaTime;
+
+        if(fAddTime >= 10f)
+        {
+            fAddTime = 0f;
+
+        }
     }
+
+    void LateUpdate()
+    {
+    }
+
 }
